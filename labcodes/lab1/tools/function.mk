610822b83fd6ad683d2da780c8796dafb21bd37d
HongkunGe <hongkun@cs.unc.edu>
2018-05-07 20:13:19 -0400

    add more print to lab1 Makefile

--- labcodes/lab1/tools/function.mk
+++ labcodes/lab1/tools/function.mk
@@ -6,6 +6,11 @@ OBJPREFIX	:= __objs_
 # list all files in some directories: (#directories, #types)
 listf = $(filter $(if $(2),$(addprefix %.,$(2)),%),\
 		  $(wildcard $(addsuffix $(SLASH)*,$(1))))
+		  
+define do_listf_print
+__info__print__ := $(call listf,$(1),$(2))
+$(info [function] listf_print $(__info__print__))
+endef
 
 # get .o obj files: (#files[, packet])
 toobj = $(addprefix $(OBJDIR)$(SLASH)$(if $(2),$(2)$(SLASH)),\
@@ -21,6 +26,8 @@ packetname = $(if $(1),$(addprefix $(OBJPREFIX),$(1)),$(OBJPREFIX))
 
 # cc compile template, generate rule for dep, obj: (file, cc[, flags, dir])
 define cc_template
+$(info [function] cc_template todep $(call todep,$(1),$(4)))
+$(info [function] cc_template toobj $(call toobj,$(1),$(4)))
 $$(call todep,$(1),$(4)): $(1) | $$$$(dir $$$$@)
 	@$(2) -I$$(dir $(1)) $(3) -MM $$< -MT "$$(patsubst %.d,%.o,$$@) $$@"> $$@
 $$(call toobj,$(1),$(4)): $(1) | $$$$(dir $$$$@)
@@ -32,6 +39,7 @@ endef
 # compile file: (#files, cc[, flags, dir])
 define do_cc_compile
 $$(foreach f,$(1),$$(eval $$(call cc_template,$$(f),$(2),$(3),$(4))))
+$$(foreach f,$(1),$$(info [function] do_cc_compile $$(call cc_template,$$(f),$(2),$(3),$(4))))
 endef
 
 # add files to packet: (#files, cc[, flags, packet, dir])
@@ -43,6 +51,8 @@ endif
 __temp_objs__ := $(call toobj,$(1),$(5))
 $$(foreach f,$(1),$$(eval $$(call cc_template,$$(f),$(2),$(3),$(5))))
 $$(__temp_packet__) += $$(__temp_objs__)
+$(info [function] do_add_files_to_packet __temp_packet__ $(__temp_packet__))
+$(info [function] do_add_files_to_packet __temp_packet__ $($(__temp_packet__))) # Question: why is this deferred?
 endef
 
 # add objs to packet: (#objs, packet)
@@ -52,6 +62,7 @@ ifeq ($$(origin $$(__temp_packet__)),undefined)
 $$(__temp_packet__) :=
 endif
 $$(__temp_packet__) += $(1)
+$(info [function] do_add_objs_to_packet __temp_packet__ $(__temp_packet__))
 endef
 
 # add packets and objs to target (target, #packes, #objs[, cc, flags])
@@ -67,6 +78,13 @@ $$(__temp_target__): $$(__temp_objs__) | $$$$(dir $$$$@)
 endif
 endef
 
+define do_print_target_info
+$(info [function] do_print_target_info __temp_packet__  $(__temp_packet__))
+$(info [function] do_print_target_info __temp_target__  $(__temp_target__))
+$(info [function] do_print_target_info __temp_objs__ $(__temp_objs__))
+$(info [function] do_print_target_info TARGETS $(TARGETS))
+$(info [function] do_print_target_info ALLOBJS $(ALLOBJS))
+endef
 # finish all
 define do_finish_all
 ALLDEPS = $$(ALLOBJS:.o=.d)
@@ -93,3 +111,6 @@ add_dependency = $(eval $(1): $(2))
 
 finish_all = $(eval $(call do_finish_all))
 
+# ------- print info -------
+listf_print = $(eval $(call do_listf_print,$(1),$(2)))
+target_print = $(eval $(call do_print_target_info))
